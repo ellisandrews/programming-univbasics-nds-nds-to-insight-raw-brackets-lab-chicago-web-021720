72f1ed1738a8c318ff47ea0415e1a237ea5f2d0d
Ellis Andrews <ellisandrews1@gmail.com>
2020-01-21 01:52:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,7 +21,19 @@ def directors_totals(nds)
 
   # pp nds
   
-  while 
+  hash = {
+  "1st Director's Name"=>1234567890,
+  "2nd Director's Name"=>1234577890,
+  "3rd Director's Name"=>1234709136,
+  ...
+}
+  
+  hash = {}
+  
+  index = 0
+  while index < nds.length do
+    
+  end
   
 
 end
